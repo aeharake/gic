43c4b69ea841ea5a49e2b39dc36c6656793a43d9
Ahmad El Harake <aeharake@gmail.com>
2021-03-09 01:13:52 +0200

    Code indentations.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require("dotenv").config()
 const createIssues = require('./createIssues');
 
-const TOKEN = process.env.TOKEN;
-const owner = process.env.OWNER;
-const repo = process.env.REPO;
-const filename = process.env.FILENAME
+const TOKEN     = process.env.TOKEN;
+const owner     = process.env.OWNER;
+const repo      = process.env.REPO;
+const filename  = process.env.FILENAME
 
 const data = {
     token: TOKEN,
