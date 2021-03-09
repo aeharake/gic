1fd70c1e33e0eadfe7a71b584fd8a6c01e9e85e7
Ahmad El Harake <aeharake@gmail.com>
2021-03-09 02:06:13 +0200

    Tab separator for csv instead of comma, and submitting after having all rows ready.

--- createIssues.js
+++ createIssues.js
@@ -1,12 +1,31 @@
 const {request} = require("@octokit/request")
 const csv = require('csv-parser');
 const fs = require('fs');
+const rows = [];
 
 module.exports = (filename, data) => {
     fs.createReadStream(filename)
-        .pipe(csv())
+        .pipe(csv({separator:'\t'}))
         .on('data', async (row) => {
-            console.log(row);
+            try {
+                rows.push(row);
+                await new Promise(resolve => setTimeout(resolve, 1500));
+            } catch (e) {
+                console.log(e.message)
+            }
+
+        })
+        .on('end', async () => {
+            console.log('Done parsing. Submitting...');
+            await submit(data, rows)
+        });
+
+}
+
+const submit = async (data, rows) => {
+    const failed = [];
+    for (const row of rows) {
+        try {
             await request(`POST /repos/${data.owner}/${data.repo}/issues`, {
                 headers: {
                     authorization: `token ${data.token}`,
@@ -14,8 +33,13 @@ module.exports = (filename, data) => {
                 ...row,
                 assignees: [...row.assignees.split(",")]
             })
-        })
-        .on('end', () => {
-            console.log('Done');
-        });
+            console.log(row)
+        } catch (e) {
+            failed.push(row);
+            console.log("Failed to submit: ")
+            console.log(row)
+        }
+    }
+    console.log("Failed to submit the following:");
+    console.log(failed);
 }
